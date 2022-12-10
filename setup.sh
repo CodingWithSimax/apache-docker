2677b4ed900fda82748f676e62894a57990a54d4
Simax <simaxme@gmail.com>
2022-12-10 14:28:22 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # bash ./build.sh
-apt install -y apache2 python3-certbot-apache
+#apt install -y  python3-certbot-apache
+apt install certbot
 certbot certonly --standalone -d simax-dev.net -d www.simax-dev.net -d portainer.simax-dev.net 
 
 # Destination path for certificates: /etc/letsencrypt/live/simax-dev.net/fullchain.pem
