1f662963042ed0b9024a898598e32bdd43b120b3
Simax <simaxme@gmail.com>
2022-12-10 13:59:44 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,11 @@ WORKDIR /etc/apache2/sites-available
 
 COPY ./sites /etc/apache2/sites-available
 
+RUN a2enmod http
+RUN a2enmod http_proxy
+
+RUN a2ensite portainer.conf
+RUN a2ensite portainer-ssl.conf
+
 EXPOSE 80 80
 EXPOSE 443 443
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-bash ./build.sh
-apt install apache2 python3-certbot-apache
+# bash ./build.sh
+apt install -y apache2 python3-certbot-apache
 certbot certonly --standalone -d simax-dev.net -d www.simax-dev.net -d portainer.simax-dev.net 
+
+# Destination path for certificates: /etc/letsencrypt/live/simax-dev.net/fullchain.pem
